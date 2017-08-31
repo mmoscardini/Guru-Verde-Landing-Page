da77894a008a445974b159b086c16053135fec99
Matheus Moscardini <matheus_moscardini@yahoo.com.br>
2017-08-31 17:14:13 -0300

    Basic node setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set ('port', (process.env.PORT || 5000));
+
+//Set path to static files
+app.use(express.static(__dirname + '/public'));
+
+//Set path to view and view engine to ejs
+app.set('views', __dirname +'/views');
+app.set ('view engine', 'ejs');
+
+app.get ('/', (request, response, next)=>{
+    response.send('Root');
+})
+
+app.listen(app.get('port'), ()=>{
+    console.log ('Server started on port: ' + app.get('port'));
+})
